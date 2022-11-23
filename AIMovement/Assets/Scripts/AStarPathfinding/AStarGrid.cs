e2b34e52253f85a5e84fe8bb9e7a836fcf08d944
Acrobaticbag3 <kevintom214@gmail.com>
2022-11-23 18:38:09 +0100

    The rework is underway
    The old AStar is gone, started working on the new one. New grid system is now done

---
+++ AIMovement/Assets/Scripts/AStarPathfinding/AStarGrid.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarGrid : MonoBehaviour {
+
+    // public Transform player; proof that grid track works
+    
+    AStarNode[,] grid;                  // 2D Node array representing our grid
+    public Vector2 sizeOfWorldGrid;     // Represents the coardinates of our grid
+    public float radiusOfNode;          // Size of induvidual node
+    public LayerMask unwalkableMask;
+
+    float diamaterOfNode;
+    int gridSizeX, gridSizeY;
+
+    void Start() {     
+        diamaterOfNode = radiusOfNode * 2;
+        gridSizeX = Mathf.RoundToInt(sizeOfWorldGrid.x/diamaterOfNode * 2);     // Gives us the amount of nodes that can fit in our grid for x axis
+        gridSizeY = Mathf.RoundToInt(sizeOfWorldGrid.y/diamaterOfNode * 2);     // Gives us the amount of nodes that can fit in our grid for y axis
+
+        CreateGrid();
+    }
+
+
+    void CreateGrid() {
+        grid = new AStarNode[gridSizeX,gridSizeY];
+        Vector3 bottomLeftOfWorld = transform.position - Vector3.right * sizeOfWorldGrid.x / 2 - Vector3.forward * sizeOfWorldGrid.y / 2; 
+
+        for(int x = 0; x < gridSizeX; x++) {
+            for(int y = 0; y < gridSizeY; y++) {
+                Vector3 worldPoint = bottomLeftOfWorld + Vector3.right * (x * diamaterOfNode * radiusOfNode) + Vector3.forward * (y * diamaterOfNode * radiusOfNode);       // Get the world position of our node collision
+                bool isWalkable = !(Physics.CheckSphere(worldPoint, radiusOfNode, unwalkableMask));                                                                         // Check for collision     
+                grid[x,y] = new AStarNode(isWalkable, worldPoint);
+            }
+        }
+    }
+
+
+    public AStarNode NodeFromWorldPoint(Vector3 nodeWorldPosition) {                                // Finds a specific node, say the one the player is standing on
+        float percentX = (nodeWorldPosition.x + sizeOfWorldGrid.x / 2) / sizeOfWorldGrid.x;         // Get position for x
+        float percentY = (nodeWorldPosition.z + sizeOfWorldGrid.y / 2) / sizeOfWorldGrid.y;         // Get position for y
+        percentX = Mathf.Clamp01(percentX);                                                         // Clamp x between 0 - 1
+        percentY = Mathf.Clamp01(percentY);                                                         // Clamp y between 0 - 1
+
+        int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+        return grid[x,y];
+    }
+
+
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(sizeOfWorldGrid.x, 1 ,sizeOfWorldGrid.y));      // Since we are using Vector2 we have to represent z axis with y
+
+        if (grid != null){ 
+            // AStarNode playerNode = NodeFromWorldPoint(player.position); // proof that grid track works
+            foreach (AStarNode nodes in grid) {
+                Gizmos.color = (nodes.isWalkable)?Color.white:Color.red;                                    // Show if node is walkable red = not walkable white = walkable
+
+                /*if (playerNode == nodes) // proof that grid track works
+                    Gizmos.color = Color.cyan;*/
+
+                Gizmos.DrawCube(nodes.nodeWorldPosition, Vector3.one * (diamaterOfNode-.1f));
+            }
+        }
+    }
+
+}
--- AIMovement/Assets/Scripts/AStarPathfinding/AStarNode.cs
+++ AIMovement/Assets/Scripts/AStarPathfinding/AStarNode.cs
@@ -1,62 +1,14 @@
-/*
-    ============================================================
-
-    This script was made by Kevin Johansson.
-
-    Additional information.
-        This script is called AStarLite Because II don't have 
-        enough time build a full A* pathfinding system from 
-        scratch.
-
-        We don't inherit from  Monobehaviour here because
-        there is no need for us to do it.
-
-    ============================================================
-*/
-
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine; 
+using UnityEngine;
 
 public class AStarNode {
-
-    // The position on our grid
-    public Vector3Int gridPosition;
-
-    // List containing node neighbours
-    public List<AStarNode> neighbours = new List<AStarNode>();
-
-    // Is the node an obstacle?
-    public bool isObstacle = false; 
-
-    // Distance from start point to goal 
-    public int gCostDistanceFromStart = 0;
-    // Distance from node to goal
-    public int hCostDistanceFromGoal = 0; 
-    // The total cots of moving to the set grid position
-    public int fCostTotal = 0;
-
-    // The order that the nodes was picked in
-    public int pickedOrder = 0;
-
-    // State to check if the cost has already been calculated
-    bool isCostCalculated = false;
     
-    // This will be called as our constructor
-    public AStarNode(Vector3Int _gridPosition) {
-        gridPosition = _gridPosition;
-    }
+    public bool isWalkable;               // Two node states, walkable or unwalkable
+    public Vector3 nodeWorldPosition;     // What point in the world does the node have?
 
-    public void CalculateCostsForNode(Vector3Int aiPosition, Vector3Int aiDestination) {
-
-        // Don't calculate the cost twice
-        if (isCostCalculated)
-            return;
-    
-        gCostDistanceFromStart = Mathf.Abs(value: gridPosition.x - aiPosition.x) + Mathf.Abs(value: gridPosition.z - aiPosition.z);
-        hCostDistanceFromGoal = Mathf.Abs(value: gridPosition.x - aiDestination.x) + Mathf.Abs(value: gridPosition.z - aiDestination.z);
-        fCostTotal = gCostDistanceFromStart + hCostDistanceFromGoal;
-        isCostCalculated = true;
-    }
+    public AStarNode(bool _isWalkable, Vector3 _nodeWorldPosition) {      // Assign values to new node
+        isWalkable = _isWalkable;
+        nodeWorldPosition = _nodeWorldPosition;
+    }                                      
 }
-
